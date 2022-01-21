5c2531e1af8d56294614f118d26ba5a04c20182a
范明毅 <fanmingyi@lizhi.fm>
2022-01-21 19:56:39 +0800

    更新版本号

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 val artifactId="FastBuilder"
 val groupName = "io.github.tiyateam.fastbuilder"
-val artifactVersion = "1.0.86"
+val artifactVersion = "2.0.0"
 group = groupName
 version = artifactVersion
 
